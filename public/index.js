536151759a5d1d3e45858096fad7a814202c5d25
Sands-45 <sandsqa@hotmail.com>
2021-09-19 15:15:20 +0200

    update

--- public/index.js
+++ public/index.js
@@ -1,14 +1,17 @@
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
+const contents = document.getElementById("contents");
 
 menuBtn.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
     menu.classList.add("block");
+    contents.classList.add("opacity-60");
     menuBtn.src = "./img/icon-close.svg";
   } else if (menu.classList.contains("block")) {
     menu.classList.remove("block");
     menu.classList.add("hidden");
     menuBtn.src = "./img/icon-hamburger.svg";
+    contents.classList.remove("opacity-60");
   }
 });
