a69c2cee3fb4d33c32721039835a98be15dc0ddd
Sands-45 <sandsqa@hotmail.com>
2021-09-19 15:22:14 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        "intro-Desktop": "url('/public/img/bg-intro-desktop.svg')",
-        "intro-Mobile": "url('/public/img/bg-intro-mobile.svg')",
-      },
       backgroundSize: {
         deskSize: "60rem",
       },
